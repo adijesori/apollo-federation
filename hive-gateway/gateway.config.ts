4eb41acd0db77e91df658672e4bdac7ffd683c05
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-01-14 23:14:04 +0300

    Add sandbox instead of graphiql

--- hive-gateway/gateway.config.ts
+++ hive-gateway/gateway.config.ts
@@ -1,17 +1,46 @@
-import { defineConfig } from '@graphql-hive/gateway';
-import { useGraphQLMiddleware } from '@envelop/graphql-middleware';
+import { defineConfig, GatewayPlugin } from "@graphql-hive/gateway";
+import { useGraphQLMiddleware } from "@envelop/graphql-middleware";
+
+const ApolloSandboxPlugin: GatewayPlugin = {
+  onRequest({ fetchAPI, request, url, endResponse }) {
+    if (
+      request.headers.get("accept")?.includes("text/html") &&
+      url.pathname === "/graphql"
+    ) {
+      return endResponse(
+        new fetchAPI.Response(
+          `
+                    <div style="width: 100%; height: 100%;" id='embedded-sandbox'></div>
+                    <script src="https://embeddable-sandbox.cdn.apollographql.com/_latest/embeddable-sandbox.umd.production.min.js"></script> 
+                    <script>
+                        new window.EmbeddedSandbox({
+                            target: '#embedded-sandbox',
+                            initialEndpoint: new URL(location.pathname, location.href).toString(),
+                        });
+                    </script>
+            `,
+          {
+            headers: {
+              "content-type": "text/html",
+            },
+          }
+        )
+      );
+    }
+  },
+};
 
 export const gatewayConfig = defineConfig({
-    plugins: () => [
-        useGraphQLMiddleware([
-            loggingMiddleware
-        ])
-    ]
-})
+  plugins: () => [
+    useGraphQLMiddleware([loggingMiddleware]),
+    ApolloSandboxPlugin,
+  ],
+  graphiql: false,
+});
 
 async function loggingMiddleware(resolve, root, args, context, info) {
-    console.log("Arguments:", args);
-    const result = await resolve(root, args, context, info);
-    console.log("Result:", result);
-    return result;
+  console.log("Arguments:", args);
+  const result = await resolve(root, args, context, info);
+  console.log("Result:", result);
+  return result;
 }
